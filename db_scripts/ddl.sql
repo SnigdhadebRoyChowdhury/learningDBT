05159323809e1c95c061f2027ecad3f663c176f2
SnigdhadebRoyChowdhury <snigdhadeb_roychowdhury@outlook.com>
2025-02-19 22:37:48 +0530

    Added data and scripts to create and insert data

---
+++ db_scripts/ddl.sql
@@ -0,0 +1,17 @@
+/* Creating the database */
+CREATE DATABASE dbt;
+
+
+/* Creating the schema */
+CREATE SCHEMA airbnb;
+
+CREATE TABLE IF NOT EXISTS airbnb.raw_listings
+                    (id bigint,
+                     listing_url text,
+                     name text,
+                     room_type text,
+                     minimum_nights text,
+                     host_id bigint,
+                     price float,
+                     created_at timestamp without time zone,
+                     updated_at timestamp without time zone);
