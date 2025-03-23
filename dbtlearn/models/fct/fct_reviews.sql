0715c8dae10e83e39a216af57943584a9415dba5
SnigdhadebRoyChowdhury <snigdhadeb_roychowdhury@outlook.com>
2025-03-23 11:15:17 +0530

    Added commnets in the fct_reviews model

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -10,6 +10,11 @@ WITH src_reviews AS (
 SELECT * FROM src_reviews 
 WHERE review_text IS NOT NULL
 
+/* The below macro checks if the model is running in incremental or not*/
 {% if is_incremental() %}
+/* 
+    The below condition checks if the review_date of the source model (src_reviews) is greater than the 
+    mximum review_date of this (fct_reviews) model
+*/
     AND review_date > (select max(review_date) from {{ this }})
 {% endif %}
