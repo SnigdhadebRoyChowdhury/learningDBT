9fd620105705f880a998fee7c8dd5c78d1364c64
Snigdhadeb Roy Chowdhury <snigdhadeb_roychowdhury@outlook.com>
2025-03-29 19:24:07 +0530

    Created the first mcro and implemented a test using the same

--- dbtlearn/models/dim/dim_listings_cleansed.sql
+++ dbtlearn/models/dim/dim_listings_cleansed.sql
@@ -6,8 +6,7 @@
 WITH src_listings AS (
     SELECT * FROM {{ ref('src_listings')}}
 )
-
-SELECT 
+SELECT
  listing_id,
  listing_name,
  room_type,
@@ -16,7 +15,7 @@ SELECT
     ELSE minimum_nights::INT
  END AS minimum_nights,
  host_id,
- price_str::DECIMAL(10,2) AS price,
+ COALESCE(price_str, 0)::DECIMAL(10,2) AS price,
  created_at,
  updated_at
 FROM src_listings
